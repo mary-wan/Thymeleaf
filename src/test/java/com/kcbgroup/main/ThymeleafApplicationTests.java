4926a5de9f157082c2ffd733742019111c5b186e
Mary Njenga <87478947+mary-wan@users.noreply.github.com>
2023-05-09 12:05:01 +0300

    remove tests

--- src/test/java/com/kcbgroup/main/ThymeleafApplicationTests.java
+++ src/test/java/com/kcbgroup/main/ThymeleafApplicationTests.java
@@ -3,11 +3,11 @@ package com.kcbgroup.main;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class ThymeleafApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
 }
